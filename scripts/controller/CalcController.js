e292bb1c35fed284ef34952ddabe41198f27cedf
K-Schaeffer <kcb.schaeffer@gmail.com>
2020-02-17 13:38:15 -0300

    CalcController initial setup

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,29 @@
 class CalcController{
  
+    constructor(){
+
+        /* Declarando os atributos */
+        this._displayCalc = "0"; //_ é o private, uma convensão
+        this._dataAtual;
+
+    }
     
-    
+    /* Getters e Setters */
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set dislpayCalc(valor){
+        this._dislpayCalc = valor; 
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(data){
+        this._dataAtual = data;
+    }
+
 }
