42ae95b5b8d08d5bb90bdcf509ce2f823a3bdd8a
K-Schaeffer <kcb.schaeffer@gmail.com>
2020-02-18 09:35:49 -0300

    Editing display in HTML with JS

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -5,13 +5,23 @@ class CalcController{
         /* Attributes */
         this._displayCalc = "0"; //_ Underline is the private convention
         this._currentDate;
-        this.initialize;
+        this.initialize();
 
     }
     
     /* Main Method */
     initialize(){
         
+        /* Selecting the display features */
+        let displayCalcEl = document.querySelector("#display"); // El = Element
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        // DOM properties to input in HTML format
+        displayCalcEl.innerHTML = "Display"; 
+        dateEl.innerHTML = "18/02/2020"; 
+        timeEl.innerHTML = "10:30"; 
+    
     }
 
     /* Getters and Setters */
